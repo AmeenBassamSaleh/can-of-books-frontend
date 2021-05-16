d67f98857cc135223ef79f11f9b70fc1a2e3fa0c
Rula Al-qasem <79834451+RulaAlqasem@users.noreply.github.com>
2021-05-16 19:15:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,9 +29,7 @@ class App extends React.Component {
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-              <Route exact path="/profile">
-              {this.props.auth0.isAuthenticated && <Profile/>}
-              </Route>
+              
             </Switch>
             <Footer />
           </IsLoadingAndError>
